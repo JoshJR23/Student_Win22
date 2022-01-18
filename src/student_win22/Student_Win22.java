db389e2ae4fafe85c3d850cb5e366c9907aa1965
joshua <joshsoccerstar7@gmail.com>
2022-01-18 10:08:52 -0500

    first commit on jan 10

---
+++ src/student_win22/Student_Win22.java
@@ -0,0 +1,28 @@
+package student_win22;
+
+/*
+ * @author joshs
+ */
+public class Student_Win22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        char[] myWord = new char[7];
+        myWord[0] = 's';
+        myWord[1] = 't';
+        myWord[2] = 'u';
+        myWord[3] = 'd';
+        myWord[4] = 'e';
+        myWord[5] = 'n';
+        myWord[6] = 't';
+        
+        for(int i = 0;i < myWord.length;i++){
+         System.out.print(myWord[i]);
+            
+        }
+    }
+    
+}
